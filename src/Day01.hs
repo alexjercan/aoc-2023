a442c8d659b7508c69bd9988a31deccf9791c3bf
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-02 07:46:33 +0200

    format

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,23 +7,23 @@ import Data.Maybe
 maybeDigit1 :: String -> Maybe Int
 maybeDigit1 [] = Nothing
 maybeDigit1 (x : _)
-  | isDigit x = Just $ digitToInt x
-  | otherwise = Nothing
+    | isDigit x = Just $ digitToInt x
+    | otherwise = Nothing
 
 maybeDigit2 :: String -> Maybe Int
 maybeDigit2 [] = Nothing
 maybeDigit2 input@(x : _)
-  | "one" `isPrefixOf` input = Just 1
-  | "two" `isPrefixOf` input = Just 2
-  | "three" `isPrefixOf` input = Just 3
-  | "four" `isPrefixOf` input = Just 4
-  | "five" `isPrefixOf` input = Just 5
-  | "six" `isPrefixOf` input = Just 6
-  | "seven" `isPrefixOf` input = Just 7
-  | "eight" `isPrefixOf` input = Just 8
-  | "nine" `isPrefixOf` input = Just 9
-  | isDigit x = Just $ digitToInt x
-  | otherwise = Nothing
+    | "one" `isPrefixOf` input = Just 1
+    | "two" `isPrefixOf` input = Just 2
+    | "three" `isPrefixOf` input = Just 3
+    | "four" `isPrefixOf` input = Just 4
+    | "five" `isPrefixOf` input = Just 5
+    | "six" `isPrefixOf` input = Just 6
+    | "seven" `isPrefixOf` input = Just 7
+    | "eight" `isPrefixOf` input = Just 8
+    | "nine" `isPrefixOf` input = Just 9
+    | isDigit x = Just $ digitToInt x
+    | otherwise = Nothing
 
 firstAndLastDigitsNum :: (String -> Maybe Int) -> String -> Int
 firstAndLastDigitsNum maybeDigit input = head digits * 10 + last digits
--- src/Day02.hs
+++ src/Day02.hs
@@ -1,19 +1,21 @@
 module Day02 (main) where
 
-import Util.Parser (Parser, parse)
-import qualified Text.Parsec as P
 import Text.Parsec ((<|>))
+import qualified Text.Parsec as P
+import Util.Parser (Parser, parse)
 
 data Round = Round
     { red :: Int
     , green :: Int
     , blue :: Int
-    } deriving (Show)
+    }
+    deriving (Show)
 
 data Game = Game
     { index :: Int
     , rounds :: [Round]
-    } deriving (Show)
+    }
+    deriving (Show)
 
 colorP :: Parser (String, Int)
 colorP = do
@@ -23,10 +25,10 @@ colorP = do
 
 colorsToRound :: [(String, Int)] -> Round
 colorsToRound cs = Round r g b
-    where
-        r = sum $ map snd $ filter ((== "red") . fst) cs
-        g = sum $ map snd $ filter ((== "green") . fst) cs
-        b = sum $ map snd $ filter ((== "blue") . fst) cs
+  where
+    r = sum $ map snd $ filter ((== "red") . fst) cs
+    g = sum $ map snd $ filter ((== "green") . fst) cs
+    b = sum $ map snd $ filter ((== "blue") . fst) cs
 
 roundP :: Parser Round
 roundP = do
@@ -56,10 +58,10 @@ part1 = show . sum . map index . filter gameValid . parseGames
 
 gameMinColors :: Game -> Round
 gameMinColors (Game _ rs) = Round r g b
-    where
-        r = maximum $ map red rs
-        g = maximum $ map green rs
-        b = maximum $ map blue rs
+  where
+    r = maximum $ map red rs
+    g = maximum $ map green rs
+    b = maximum $ map blue rs
 
 power :: Round -> Int
 power (Round r g b) = r * g * b
