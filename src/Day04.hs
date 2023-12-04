18533da78932f51710cf960f796682ffb6c6f953
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-04 07:58:00 +0200

    format

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,7 +1,7 @@
-module Day04 (main) where
+module Day04 (main, part1, part2) where
 
-import qualified Text.Parsec as P
 import Data.List (sort)
+import qualified Text.Parsec as P
 import Util.Parser (Parser, parse)
 
 data Card = Card [Int] [Int]
@@ -20,14 +20,14 @@ parseCards = parse (P.many1 cardP)
 
 matches :: Card -> Int
 matches (Card winning numbers) = go winning numbers 0
-    where
-        go [] [] acc = acc
-        go _ [] acc = acc
-        go [] _ acc = acc
-        go ws'@(w:ws) ns'@(n:ns) acc
-            | w == n = go ws ns (acc + 1)
-            | w > n = go ws' ns acc
-            | otherwise = go ws ns' acc
+  where
+    go [] [] acc = acc
+    go _ [] acc = acc
+    go [] _ acc = acc
+    go ws'@(w : ws) ns'@(n : ns) acc
+        | w == n = go ws ns (acc + 1)
+        | w > n = go ws' ns acc
+        | otherwise = go ws ns' acc
 
 score :: Int -> Int
 score m = if m == 0 then 0 else 2 ^ (m - 1)
@@ -37,10 +37,10 @@ part1 = show . sum . map (score . matches) . parseCards
 
 count :: [Card] -> Int
 count cs = sum $ foldl go (replicate (length ms) 1) (zip ms [1 ..])
-    where
-        ms = map matches cs
-        go :: [Int] -> (Int, Int) -> [Int]
-        go acc (m, i) = zipWith (\a j -> if j `elem` [i + 1 .. i + m] then a + (acc !! (i - 1)) else a) acc [1 ..]
+  where
+    ms = map matches cs
+    go :: [Int] -> (Int, Int) -> [Int]
+    go acc (m, i) = zipWith (\a j -> if j `elem` [i + 1 .. i + m] then a + (acc !! (i - 1)) else a) acc [1 ..]
 
 part2 :: String -> String
 part2 = show . count . parseCards
