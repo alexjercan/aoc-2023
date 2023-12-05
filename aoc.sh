2137914654e4930a4be5f4836b02b85045e96d98
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-05 17:22:44 +0200

    yeet

--- aoc.sh
+++ aoc.sh
@@ -4,7 +4,7 @@ Color_Off='\033[0m'
 
 IRed='\033[0;91m'
 IGreen='\033[0;92m'
-IWhite='\033[0;97m'
+IYellow='\033[0;93m'
 
 BIGreen='\033[1;92m'
 
@@ -13,18 +13,31 @@ bash get
 stack build
 
 echo -e "${BIGreen}Advent of Code 2023${Color_Off}"
-echo
+
+star="\e[5;33m*\e[0m\e[1;32m"
+o="\e[0m\e[1;31mo\e[0m\e[1;32m"
+
+echo -e "
+        ${star}
+       /.\\
+      /${o}..\\
+      /..${o}\\
+     /.${o}..${o}\\
+     /...${o}.\\
+    /..${o}....\\
+    ^^^[_]^^^
+"
 
 echo -e "${IRed}--- Day 1: Trebuchet?! ---${Color_Off}"
 stack exec day01 < input/day01.input
 
-echo -e "${IWhite}--- Day 2: Cube Conundrum ---${Color_Off}"
+echo -e "${IYellow}--- Day 2: Cube Conundrum ---${Color_Off}"
 stack exec day02 < input/day02.input
 
 echo -e "${IGreen}--- Day 3: Gear Ratios ---${Color_Off}"
 stack exec day03 < input/day03.input
 
-echo -e "${IWhite}--- Day 4: Scratchcards ---${Color_Off}"
+echo -e "${IYellow}--- Day 4: Scratchcards ---${Color_Off}"
 stack exec day04 < input/day04.input
 
 echo -e "${IRed}--- Day 5: If You Give A Seed A Fertilizer ----${Color_Off}"
