25b8672b21a66eecc66d5d0ee0e2599359ff8b18
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-09 09:37:46 +0200

    refactor

--- src/Day09.hs
+++ src/Day09.hs
@@ -6,29 +6,19 @@ parse = map (map read . words) . lines
 diff :: [Int] -> [Int]
 diff = zipWith (-) <$> tail <*> id
 
-prediction :: [Int] -> Int
-prediction xs = fst $ until stop step (0, xs)
+prediction :: (Int -> [Int] -> Int) -> [Int] -> Int
+prediction f xs = foldl f 0 $ fst $ until stop step ([], xs)
   where
-    step :: (Int, [Int]) -> (Int, [Int])
-    step (p, ys) = (p + last ys, diff ys)
-    stop :: (Int, [Int]) -> Bool
+    step (ps, ys) = (ys : ps, diff ys)
     stop (_, ys) = all (== 0) ys
 
 part1 :: String -> String
-part1 = show . sum . map prediction . parse
-
-prediction' :: [Int] -> Int
-prediction' xs = foldl merge 0 $ fst $ until stop step ([], xs)
-  where
-    step :: ([Int], [Int]) -> ([Int], [Int])
-    step (ps, ys) = (head ys : ps, diff ys)
-    stop :: ([Int], [Int]) -> Bool
-    stop (_, ys) = all (== 0) ys
-    merge :: Int -> Int -> Int
-    merge acc x = x - acc
+part1 = show . sum . map (prediction merge) . parse
+    where merge acc xs = acc + last xs
 
 part2 :: String -> String
-part2 = show . sum . map prediction' . parse
+part2 = show . sum . map (prediction merge) . parse
+    where merge acc xs = head xs - acc
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input ++ "\n"
