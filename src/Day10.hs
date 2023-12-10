72de45c8ccd292f087a86f2671c6f1e786067c78
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-10 13:10:55 +0200

    day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,7 +1,9 @@
 module Day10 (main, part1, part2) where
 
+import Control.Arrow ((&&&))
+
 findS :: [String] -> (Int, Int)
-findS input = head $ filter (\(x, y) -> (input !! y) !! x == 'S')  [(x, y) | x <- [0..length (head input) - 1], y <- [0..length input - 1]]
+findS input = head $ filter (\(x, y) -> (input !! y) !! x == 'S') [(x, y) | x <- [0 .. length (head input) - 1], y <- [0 .. length input - 1]]
 
 checkPipe :: Char -> Char -> Char -> Bool
 checkPipe '|' '|' 'N' = True
@@ -66,6 +68,18 @@ checkPipe 'S' 'L' 'W' = True
 checkPipe 'S' 'F' 'W' = True
 checkPipe _ _ _ = False
 
+replaceS :: [String] -> (Int, Int) -> Char
+replaceS input (x, y)
+    | dirs == "NE" = 'J'
+    | dirs == "NW" = 'L'
+    | dirs == "ES" = '7'
+    | dirs == "SW" = 'F'
+    | dirs == "NS" = '|'
+    | dirs == "EW" = '-'
+    | otherwise = error "Invalid pipe"
+  where
+    dirs = map deltaToDir $ pipeNeighbors input (x, y)
+
 deltaToDir :: (Int, Int) -> Char
 deltaToDir (0, -1) = 'N'
 deltaToDir (1, 0) = 'E'
@@ -76,36 +90,61 @@ deltaToDir _ = 'X'
 at :: [String] -> (Int, Int) -> Char
 at input (x, y) = (input !! y) !! x
 
+replaceAt :: [String] -> (Int, Int) -> Char -> [String]
+replaceAt input (x, y) c = take y input ++ [take x row ++ [c] ++ drop (x + 1) row] ++ drop (y + 1) input
+  where
+    row = input !! y
+
 validDirs :: [String] -> (Int, Int) -> [(Int, Int)]
 validDirs input (x, y) = filter check dirs
-    where
-        dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-        check (dx, dy) = x + dx >= 0 && x + dx < length (head input) && y + dy >= 0 && y + dy < length input
+  where
+    dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    check (dx, dy) = x + dx >= 0 && x + dx < length (head input) && y + dy >= 0 && y + dy < length input
 
 pipeNeighbors :: [String] -> (Int, Int) -> [(Int, Int)]
-pipeNeighbors input (x, y) = map add $ filter check $ validDirs input (x, y)
-    where
-        check (dx, dy) = checkPipe (at input (x, y)) (at input (x + dx, y + dy)) (deltaToDir (dx, dy))
-        add (dx, dy) = (x + dx, y + dy)
+pipeNeighbors input (x, y) = filter check $ validDirs input (x, y)
+  where
+    check (dx, dy) = checkPipe (at input (x, y)) (at input (x + dx, y + dy)) (deltaToDir (dx, dy))
 
 dfs :: [String] -> [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
 dfs _ [] acc = acc
-dfs input ((x, y):queue) acc
+dfs input ((x, y) : queue) acc
     | (x, y) `elem` acc = dfs input queue acc
     | otherwise = dfs input queue' acc'
-    where
-        neighbors = pipeNeighbors input (x, y)
-        queue' = queue ++ neighbors
-        acc' = (x, y):acc
+  where
+    neighbors = map add $ pipeNeighbors input (x, y)
+    queue' = queue ++ neighbors
+    acc' = (x, y) : acc
+    add (dx, dy) = (x + dx, y + dy)
+
+mkMap :: [String] -> [(Int, Int)] -> [((Int, Int), Char)]
+mkMap input = map (id &&& at input)
+
+isInPolygon :: [((Int, Int), Char)] -> (Int, Int) -> (Int, Int) -> Bool
+isInPolygon poly (w, h) (sx, sy) = odd direction
+  where
+    direction = length $ filter (not . corner . snd) $ filter ((`elem` zip [sx .. w] [sy .. h]) . fst) poly
+    corner c = c == 'L' || c == '7'
 
 part1 :: String -> String
 part1 input = show $ (`div` 2) $ length $ dfs m [(sx, sy)] []
-    where
-        (sx, sy) = findS m
-        m = lines input
+  where
+    m = lines input
+    (sx, sy) = findS m
 
 part2 :: String -> String
-part2 = const ""
+part2 input = show $ length inner
+  where
+    m = lines input
+    (sx, sy) = findS m
+    m' = replaceAt m (sx, sy) $ replaceS m (sx, sy)
+    poly = dfs m' [(sx, sy)] []
+    xMin = minimum $ map fst poly
+    xMax = maximum $ map fst poly
+    yMin = minimum $ map snd poly
+    yMax = maximum $ map snd poly
+    points = [(x, y) | x <- [xMin .. xMax], y <- [yMin .. yMax], (x, y) `notElem` poly]
+    inner = filter (isInPolygon (mkMap m' poly) (xMax, yMax)) points
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input ++ "\n"
--- test/TestDay10.hs
+++ test/TestDay10.hs
@@ -3,14 +3,15 @@ module TestDay10 (test) where
 import Day10
 import Test.Hspec
 
-input :: String
-input = "..F7.\n.FJ|.\nSJ.L7\n|F--J\nLJ..."
-
 test1 :: Expectation
 test1 = part1 input `shouldBe` "8"
+  where
+    input = "..F7.\n.FJ|.\nSJ.L7\n|F--J\nLJ..."
 
 test2 :: Expectation
 test2 = part2 input `shouldBe` "10"
+  where
+    input = "FF7FSF7F7F7F7F7F---7\nL|LJ||||||||||||F--J\nFL-7LJLJ||||||LJL-77\nF--JF--7||LJLJ7F7FJ-\nL---JF-JLJ.||-FJLJJ7\n|F|F-JF---7F7-L7L|7|\n|FFJF7L7F-JF7|JL---7\n7-L-JL7||F7|L7F-7F7|\nL.L7LFJ|||||FJL7||LJ\nL7JLJL-JLJLJL--JLJ.L"
 
 test :: IO ()
 test = hspec $ do
