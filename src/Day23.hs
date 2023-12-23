2530b4e97a40dc5bfa6c788bf7704a9ed363b427
Alex Jercan <jercan_alex27@yahoo.com>
2023-12-23 12:27:16 +0200

    trash... still no

--- src/Day23.hs
+++ src/Day23.hs
@@ -36,15 +36,20 @@ parse input = (starting, target, (M.fromList positions, n))
     target = head $ filter ((== n - 1) . fst . fst) positions
     n = length $ lines input
 
+parse' :: String -> ((Position, Dir), (Position, Dir), Grid)
+parse' input = (starting, target, (M.fromList positions, n))
+  where
+    positions = map (fmap (const C)) $ coords input (/= '#')
+    starting = head $ filter ((== 0) . fst . fst) positions
+    target = head $ filter ((== n - 1) . fst . fst) positions
+    n = length $ lines input
+
 neighbors :: Grid -> (Position, Dir) -> [(Position, Dir)]
 neighbors (grid, _) ((r, c), U) = [(p, d) | p <- [(r - 1, c)], Just d <- [M.lookup p grid]]
 neighbors (grid, _) ((r, c), D) = [(p, d) | p <- [(r + 1, c)], Just d <- [M.lookup p grid]]
 neighbors (grid, _) ((r, c), L) = [(p, d) | p <- [(r, c - 1)], Just d <- [M.lookup p grid]]
 neighbors (grid, _) ((r, c), R) = [(p, d) | p <- [(r, c + 1)], Just d <- [M.lookup p grid]]
 neighbors (grid, _) ((r, c), C) = [(p, d) | p <- [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)], Just d <- [M.lookup p grid]]
-
-neighbors' :: Grid -> (Position, Dir) -> [(Position, Dir)]
-neighbors' (grid, _) ((r, c), _) = [(p, d) | p <- [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)], Just d <- [M.lookup p grid]]
 
 edge :: Grid -> (Position, Dir) -> (Position, Dir) -> Int -> ((Position, Dir) -> [(Position, Dir)]) -> S.Set (Position, Dir) -> Maybe ((Position, Dir), Int)
 edge grid t p l neighborsF visited
@@ -62,24 +67,24 @@ graph grid t p neighborsF visited
         [] -> M.empty
         ns -> M.insert p ns $ M.unions $ map (\(n, _) -> graph grid t n neighborsF (S.insert p visited)) ns
 
-bfs :: M.Map (Position, Dir) [((Position, Dir), Int)] -> (Position, Dir) -> (Position, Dir) -> S.Set (Position, Dir) -> Int
-bfs gs p t visited
+dfs :: M.Map (Position, Dir) [((Position, Dir), Int)] -> (Position, Dir) -> (Position, Dir) -> S.Set (Position, Dir) -> Int
+dfs gs p t visited
     | p == t = 0
     | otherwise = case M.lookup p gs of
         Nothing -> 0
-        Just ns -> maximum (map (\(p', l) -> l + bfs gs p' t (S.insert p' visited)) ns)
+        Just ns -> maximum (map (\(p', l) -> l + dfs gs p' t (S.insert p' visited)) ns)
 
 part1 :: String -> String
-part1 input = show $ bfs gs starting target S.empty
+part1 input = show $ dfs gs starting target S.empty
   where
     (starting, target, grid) = parse input
     gs = graph grid target starting (neighbors grid) S.empty
 
 part2 :: String -> String
-part2 input = show $ bfs gs starting target S.empty
+part2 input = show $ dfs gs starting target S.empty
   where
-    (starting, target, grid) = parse input
-    gs = graph grid target starting (neighbors' grid) S.empty
+    (starting, target, grid) = parse' input
+    gs = graph grid target starting (neighbors grid) S.empty
 
 solve :: String -> String
 solve input = "Part 1: " ++ part1 input ++ "\nPart 2: " ++ part2 input ++ "\n"
