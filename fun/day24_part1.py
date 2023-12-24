4a275788e2138bb8f6d011cdddd9f9d69406c8de
Alex Jercan <jerca_alex27@yahoo.com>
2023-12-24 12:55:56 +0200

    still doesn;t fing work??? why what is wrong with haskell floats?

---
+++ fun/day24_part1.py
@@ -0,0 +1,58 @@
+def parse_line(line: str):
+    pos_str, vel_str = line.strip().split(" @ ", 1)
+    pos = tuple(map(int, pos_str.split(",")))
+    vel = tuple(map(int, vel_str.split(",")))
+    return pos, vel
+
+def parse(input_str: str):
+    return list(map(parse_line, input_str.splitlines()))
+
+def intersect(cell1, cell2):
+    ((x1, y1, _), (dx1, dy1, _)) = cell1
+    ((x2, y2, _), (dx2, dy2, _)) = cell2
+
+    xdiff = (-dx1, -dx2)
+    ydiff = (-dy1, -dy2)
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return False
+
+    d = (det((x1, y1), (x1 + dx1, y1 + dy1)), det((x2, y2), (x2 + dx2, y2 + dy2)))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+
+    if 200000000000000 <= x <= 400000000000000 and 200000000000000 <= y <= 400000000000000 \
+            and (x - x1 > 0) == (dx1 > 0) and (y - y1 > 0) == (dy1 > 0) \
+            and (x - x2 > 0) == (dx2 > 0) and (y - y2 > 0) == (dy2 > 0):
+        return True
+
+    return False
+
+
+def part1(xs):
+    n = len(xs)
+
+    count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            cell1 = xs[i]
+            cell2 = xs[j]
+
+            if intersect(cell1, cell2):
+                count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    input_str = open("../input/day24.input").read()
+
+    # Part 1
+    xs = parse(input_str)
+
+    print(xs)
+    print(part1(xs))
--- src/Day24.hs
+++ src/Day24.hs
@@ -3,13 +3,8 @@ module Day24 (main, part1, part2) where
 import Util.Parser (Parser, parse)
 import qualified Text.Parsec as P
 
-type Vec2 = (Double, Double)
 type Vec3 = (Double, Double, Double)
-type Particle2D = (Vec2, Vec2)
-type Particle3D = (Vec3, Vec3)
-
-dropZ :: Particle3D -> Particle2D
-dropZ ((x, y, _), (dx, dy, _)) = ((x, y), (dx, dy))
+type Particle = (Vec3, Vec3)
 
 negP :: Parser Double
 negP = negate . read <$> (P.char '-' *> P.many1 P.digit)
@@ -27,40 +22,38 @@ vec3P = do
   z <- numP
   return (x, y, z)
 
-particleP :: Parser Particle3D
+particleP :: Parser Particle
 particleP = do
   p <- vec3P <* P.spaces <* P.char '@' <* P.spaces
   v <- vec3P
   return (p, v)
 
-inputP :: Parser [Particle3D]
+inputP :: Parser [Particle]
 inputP = P.many1 (particleP <* P.spaces) <* P.eof
 
-border :: (Num a, Ord a) => a -> Bool
-border x = 200000000000000 <= x && x <= 400000000000000
+intersect :: Particle -> Particle -> Bool
+intersect ((x1, y1, _), (dx1, dy1, _)) ((x2, y2, _), (dx2, dy2, _)) =
+    let xdiff = (-dx1, -dx2)
+        ydiff = (-dy1, -dy2)
+        det (a, b) = fst a * snd b - snd a * fst b
+        div' = det (xdiff, ydiff)
+        d = (det ((x1, y1), (x1 + dx1, y1 + dy1)), det ((x2, y2), (x2 + dx2, y2 + dy2)))
+        x = det (d, xdiff) / div'
+        y = det (d, ydiff) / div'
+    in  div' /= 0
+        && 200000000000000 <= x && x <= 400000000000000 && 200000000000000 <= y && y <= 400000000000000
+        && (x - x1 > 0) == (dx1 > 0) && (y - y1 > 0) == (dy1 > 0)
+        && (x - x2 > 0) == (dx2 > 0) && (y - y2 > 0) == (dy2 > 0)
 
-det :: Vec2 -> Vec2 -> Double
-det (x1, y1) (x2, y2) = x1 * y2 - x2 * y1
+combinations2 :: [a] -> [(a, a)]
+combinations2 [] = []
+combinations2 (x:xs) = map ((,) x) xs ++ combinations2 xs
 
-intersect2D :: Particle2D -> Particle2D -> Bool
-intersect2D ((x1, y1), (dx1, dy1)) ((x2, y2), (dx2, dy2)) =
-    let xd = (-dx1, -dx2)
-        yd = (-dy1, -dy2)
-        dv = det xd yd
-        d = (det (x1, y1) (x1 + dx1, y1 + dy1), det (x2, y2) (x2 + dx2, y2 + dy2))
-        x = det d xd / dv
-        y = det d yd / dv
-    in
-        dv /= 0 &&
-        (x - x1 > 0) == (dx1 > 0) && (y - y1 > 0) == (dy1 > 0) &&
-        (x - x2 > 0) == (dx2 > 0) && (y - y2 > 0) == (dy2 > 0) &&
-        border x && border y
-
-solution :: [Particle2D] -> Int
-solution particles = length $ filter (uncurry intersect2D) [(p1, p2) | p1 <- particles, p2 <- particles, p1 /= p2]
+solution :: [Particle] -> Int
+solution particles = length $ filter (uncurry intersect) $ combinations2 particles
 
 part1 :: String -> String
-part1 = show . solution . map dropZ . parse inputP
+part1 = show . solution . parse inputP
 
 part2 :: String -> String
 part2 = const ""
