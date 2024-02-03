0deeffdbf37806fea2b268e413ca3ac022641200
Alex Jercan <jerca_alex27@yahoo.com>
2024-02-03 09:45:05 +0200

    fix: remove warnings

--- src/Day25.hs
+++ src/Day25.hs
@@ -5,10 +5,7 @@ import Control.Monad.State (
     MonadState (get, put),
     State,
     evalState,
-    modify,
  )
-import Data.Function (on)
-import Data.List (minimumBy)
 import qualified Data.Map as M
 import System.Random
 import qualified Text.Parsec as P
@@ -26,10 +23,10 @@ inputP = do
     let g = M.fromList pairs
     return $ foldl (\g' (n, ns) -> foldl (\g'' n' -> M.insertWith (++) n' [n] g'') g' ns) g pairs
 
-data KargerState = KargerState (StdGen)
+newtype KargerState = KargerState StdGen
 
 choice :: StdGen -> [a] -> (a, StdGen)
-choice g [] = error "choice: empty list"
+choice _ [] = error "choice: empty list"
 choice g xs = (xs !! i, g')
   where
     (i, g') = randomR (0, length xs - 1) g
@@ -38,8 +35,8 @@ contract :: String -> String -> (M.Map String [String], M.Map String [String]) -
 contract u v (g, s) =
     let g' = M.delete v $ M.delete u g
         g'' = M.insert u (g M.! u ++ g M.! v) g'
-        g''' = M.map (\xs -> map (\x -> if x == v then u else x) xs) g''
-        g'''' = M.update (\xs -> Just $ filter (/= u) xs) u g'''
+        g''' = M.map (map (\x -> if x == v then u else x)) g''
+        g'''' = M.update (Just . filter (/= u)) u g'''
         s' = M.delete v $ M.delete u s
         s'' = M.insert u (s M.! u ++ s M.! v) s'
      in (g'''', s'')
@@ -52,7 +49,7 @@ minCutM graph s = do
         else do
             let keys = M.keys graph
             let (u, g') = choice g keys
-            let (v, g'') = choice g' $ filter (\x -> x `M.member` graph) $ graph M.! u
+            let (v, g'') = choice g' $ filter (`M.member` graph) $ graph M.! u
             let (graph', s') = contract u v (graph, s)
             put $ KargerState g''
             minCutM graph' s'
